c1e6f891fdaec0b676bdb2a99ad4c355eb2c9631
mostafa-asefy <asefi.v@gmail.com>
2020-05-23 11:56:22 +0330

    Lesson 05, Database released

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from kivymd.app import MDApp
 from servicesmapview import ServicesMapView
+import sqlite3
 
 class MainApp(MDApp):
-    pass
+    database = None
+    cursor = None
+
+    def on_start(self):
+        self.database = sqlite3.connect("mydb.sqlite")
+        self.cursor = self.database.cursor()
+
+
 
 MainApp().run()
--- servicesmapview.py
+++ servicesmapview.py
@@ -1,5 +1,6 @@
 from kivy.garden.mapview import MapView
 from kivy.clock import Clock
+from kivy.app import App
 
 class ServicesMapView(MapView):
     start_getting_markers_timer = None
@@ -13,4 +14,9 @@ class ServicesMapView(MapView):
 
 
     def getting_markers_in_fov(self, *args):
-        print("Moved!")
+        min_lat, min_lon, max_lat, max_lon = self.get_bbox()
+        app = App.get_running_app()
+        sql_stamtement = "SELECT * FROM main WHERE latitude > %s AND latitude < %s AND longitude > %s AND longitude < %s"%(min_lat, max_lat, min_lon, max_lon)
+        app.cursor.execute(sql_stamtement)
+        markets = app.cursor.fetchall()
+        print(markets)
