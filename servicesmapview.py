9567c46151c048354b93752904fb8f7a7e44a759
mostafa-asefy <asefi.v@gmail.com>
2020-06-02 12:15:58 +0330

    Lesson 07, CorrectMarker, Released

--- servicesmapview.py
+++ servicesmapview.py
@@ -5,6 +5,7 @@ from servicesmarker import ServicesMarker
 
 class ServicesMapView(MapView):
     start_getting_markers_timer = None
+    markers_name = []
 
     def start_getting_markers_in_fov(self):
         try:
@@ -20,12 +21,17 @@ class ServicesMapView(MapView):
         sql_stamtement = "SELECT * FROM main WHERE latitude > %s AND latitude < %s AND longitude > %s AND longitude < %s"%(min_lat, max_lat, min_lon, max_lon)
         app.cursor.execute(sql_stamtement)
         markers = app.cursor.fetchall()
-        print(markers)
+
         for marker in markers:
-            self.add_servicemarkers(marker)
+            if marker[1] in self.markers_name:
+                continue
+            else:
+                print(marker)
+                self.add_servicemarkers(marker)
 
 
     def add_servicemarkers(self, marker):
         lat, lon = marker[14], marker[15]
         temp_marker = ServicesMarker(lat=lat, lon=lon)
         self.add_widget(temp_marker)
+        self.markers_name.append(marker[1])
